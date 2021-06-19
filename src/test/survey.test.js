7b7364016452f31638e622e4e023afe86400e577
Melkisedek Ubalde <melkisedek222@gmail.com>
2021-06-19 16:03:54 +0800

    test 11

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -15,7 +15,6 @@ describe("Survey Model", function(){
         console.log(result);
         console.log(result);
         console.log(result);
-        console.log(result);
         expect(result).to.equal("Success! Captcha input matched.");
     });
 
